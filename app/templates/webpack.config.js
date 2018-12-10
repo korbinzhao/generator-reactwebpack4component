7c22e42640745b06957849adf77542a08aac2781
宛天 <zhaobin.zb@alibaba-inc.com>
2018-12-10 17:47:37 +0800

    add css-loader

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   resolve: {
@@ -10,65 +10,69 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.js$|\.jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+      test: /\.js$|\.jsx$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: {
+          minimize: true
+        }
+      }]
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    },
+    {
+      test: /\.less$/,
+      exclude: '/node_modules',
+      use: [{
+        loader: 'style-loader'
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1
         }
       },
       {
-        test: /\.html$/,
-        use: [{
-          loader: "html-loader",
-          options: {
-            minimize: true
-          }
-        }]
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          plugins: loader => [
+            require('postcss-import')({
+              root: loader.resourcePath
+            }),
+            require('postcss-cssnext')(),
+            require('autoprefixer')(),
+            require('cssnano')()
+          ]
+        }
       },
       {
-        test: /\.less$/,
-        exclude: '/node_modules',
-        use: [{
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: (loader) => [
-                require('postcss-import')({
-                  root: loader.resourcePath
-                }),
-                require('postcss-cssnext')(),
-                require('autoprefixer')(),
-                require('cssnano')()
-              ]
-            }
-          },
-          {
-            loader: 'less-loader',
-            options: {
-              importLoaders: 1
-            }
-          }
-        ]
-      },
-      {
-        test: /\.styl$/,
-        loader: 'style-loader!css-loader!stylus-loader'
+        loader: 'less-loader',
+        options: {
+          importLoaders: 1
+        }
       }
+      ]
+    },
+    {
+      test: /\.styl$/,
+      loader: 'style-loader!css-loader!stylus-loader'
+    }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./demo/index.html",
-      filename: "./index.html",
+      template: './demo/index.html',
+      filename: './index.html',
       chunks: ['demo']
     })
   ]
